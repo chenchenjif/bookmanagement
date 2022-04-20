3be100153ab0d0971a41e2638ed6f917514bc578
chenchenjif <87172926+chenchenjif@users.noreply.github.com>
2022-04-20 10:38:10 +0800

    delete error
    delete error

--- spring-boot/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.User2;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository <User2,Integer>{
-}
